2aea81fda82ee6d10734a1a09a787c8ade53c6a3
fumito-s <fmt.fumito@gmail.com>
2022-02-27 17:38:29 +0900

    Add Navigation Library

--- app/src/main/java/com/fumito/learninglog/LearningLogApp.kt
+++ app/src/main/java/com/fumito/learninglog/LearningLogApp.kt
@@ -2,14 +2,23 @@ package com.fumito.learninglog
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.fumito.learninglog.ui.components.LearningLogScaffold
 import com.fumito.learninglog.ui.theme.LearningLogTheme
 
 @Composable
 fun LearningLogApp() {
+    val navController = rememberNavController()
+
     LearningLogTheme {
         LearningLogScaffold {
-            Greeting("Droid")
+            NavHost(navController, startDestination = "main") {
+                composable("main") {
+                    Greeting("Droid")
+                }
+            }
         }
     }
 }
