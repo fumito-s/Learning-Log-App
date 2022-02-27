2370a5664fa9c5ddd5870bbd007c9f11ddfb0687
fumito-s <fmt.fumito@gmail.com>
2022-02-27 17:27:37 +0900

    Delete unused imports

--- app/src/main/java/com/fumito/learninglog/MainActivity.kt
+++ app/src/main/java/com/fumito/learninglog/MainActivity.kt
@@ -3,12 +3,6 @@ package com.fumito.learninglog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.fumito.learninglog.ui.theme.LearningLogTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/fumito/learninglog/ui/theme/Theme.kt
+++ app/src/main/java/com/fumito/learninglog/ui/theme/Theme.kt
@@ -5,8 +5,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
